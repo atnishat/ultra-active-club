1b0572f8b1f969b8ae13361edf9e908630b15477
atnishat <nishat.mu47@gmail.com>
2022-09-29 16:46:04 +0600

    exercise details

--- src/components/shop/Shop.js
+++ src/components/shop/Shop.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Product from '../Product/Product';
 import './Shop.css'
+import logo from '../../Images/logo2.jpg'
 const Shop = () => {
         const [products, setProducts] = useState([])
     useEffect( () => {
@@ -27,7 +28,41 @@ const Shop = () => {
         }
    </div>
             <div className='cart-container'>
-        <h3>Order summary</h3>
+                <div className='user-details'>
+                    <img src={logo} alt="" />
+                    <div className='user-info'>
+                        <h4>Tasnim Nishat</h4>
+                        <p>Sylhet,BD</p>
+                    </div>
+                </div>
+
+                <div className='total'>
+                        <div className='info'>
+                            <h6>75kg</h6>
+                            <h6>6.5</h6>
+                            <h6>25yrs</h6>
+                        </div>
+                        <div className='info-2'>
+                            <p>Weight</p>
+                            <p>Height</p>
+                            <p>Age</p>
+                        </div>
+                    </div>
+
+
+
+                <div className='break'>
+                    <h2>Add A Break</h2>
+                    <div className='break-time'>
+                        <h5>10s</h5>
+                        <h5>20s</h5>
+                        <h5>30s</h5>
+                        <h5>40s</h5>
+                        <h5>50s</h5>
+                    </div>
+                </div>
+
+
             </div>
         </div>
     );
